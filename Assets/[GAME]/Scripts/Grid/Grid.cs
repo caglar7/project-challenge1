dbdf4192ebc3ddcb6f63d005c72d4f656a69b9c3
Caglar <mcaglarcnkr@gmail.com>
2023-01-11 21:08:04 +0300

    grid generation with given dimension

--- Assets/[GAME]/Scripts/Grid/Grid.cs
+++ Assets/[GAME]/Scripts/Grid/Grid.cs
@@ -4,22 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// generation a grid with piece game objects
+/// generatinng a grid with piece game objects
 /// </summary>
 
 public class Grid : MonoBehaviour
 {
     [Header("Grid Settings")]
     [Range(1, 10)] [SerializeField] int n = 1;
-    [Tooltip("distance to edges")] [SerializeField] float margin = .1f;
+    //[Tooltip("distance to edges")] [SerializeField] float margin = .1f;
 
-    
+    Vector3 upperLeft;
+    float gridSize;
+    GridPiece[,] gridPieces;
 
     private void Start()
     {
-        GenerateGrid();
+        GenerateGrid(n);
     }
-
 
     /// <summary>
     /// generating based on n value and margin
@@ -28,9 +29,52 @@ public class Grid : MonoBehaviour
     /// width is taken as the edge of the square play area
     /// 
     /// </summary>
-    private void GenerateGrid()
+    private void GenerateGrid(int n)
     {
-        float gridEdgeSize = Screen.width;
-        float cellSize = gridEdgeSize =
+        Init();
+        ClearGrid();
+        float cellSize = gridSize / n;
+        transform.position = Vector3.zero;
+        gridPieces = new GridPiece[n, n];
+        upperLeft += new Vector3(cellSize / 2f, -cellSize / 2f, 0f);
+
+        for (int x = 0; x < n; x++)
+        {
+            for (int y = 0; y < n; y++)
+            {
+                GameObject clone = PoolManager.Instance.gridPiecePool.PullObjFromPool();
+                clone.transform.position = upperLeft + new Vector3(x * cellSize, -y * cellSize, 0f);
+                clone.transform.SetParent(transform);
+
+                gridPieces[x, y] = clone.GetComponent<GridPiece>();
+            }
+        }
+    }
+
+    /// <summary>
+    /// finding grid width in world place
+    /// </summary>
+    private void Init()
+    {
+        Transform camTransform = Camera.main.transform;
+        Vector3 screenUpperLeft = new Vector3(0f, Screen.height, 0f - camTransform.position.z);
+        upperLeft = Camera.main.ScreenToWorldPoint(screenUpperLeft);
+        gridSize = 2 * Mathf.Abs(upperLeft.x);
+    }
+
+    /// <summary>
+    /// clear just before generating
+    /// </summary>
+    private void ClearGrid()
+    {
+        if (gridPieces == null) return;
+
+        for (int x = 0; x < gridPieces.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridPieces.GetLength(1); y++)
+            {
+                PoolManager.Instance.gridPiecePool.AddObjToPool(gridPieces[x, y].gameObject);
+            }
+        }
     }
 }
--- Assets/[GAME]/Scripts/Grid/GridPiece.cs
+++ Assets/[GAME]/Scripts/Grid/GridPiece.cs
@@ -2,7 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// component for each grid piece
+/// </summary>
+
+public enum PieceState
+{
+    empty,
+    cross,
+}
+
 public class GridPiece : MonoBehaviour
 {
+    private PieceState state;
 
+    public void Init()
+    {
+        // empty sprite remove cross
+        // init any data there is
+    }
+
+    public PieceState GetState()
+    {
+        return state;
+    }
 }
